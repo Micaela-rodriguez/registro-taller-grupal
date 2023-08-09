8873ac8c7ae3075480e4df30964116c500e5b871
Micaela-rodriguez <micaela.roho@outlook.com>
2023-08-09 09:30:06 -0300

    modificaciones

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -1,7 +1,51 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const regBtn = document.getElementById("regBtn");
+  
+    regBtn.addEventListener("click", function () {
+      const data = {
+        nombre: document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        email: document.getElementById("email").value,
+        password1: document.getElementById("password1").value,
+        password2: document.getElementById("password2").value
+      };
+  
+      for (var key in data) {
+        if (data[key] === "") {
+          showAlertError("El campo " + key + " no puede estar vacío");
+          return;
+        }
+      }
+  
+      if (data.password1.length < 6) {
+        showAlertError("La contraseña debe tener al menos 6 caracteres");
+        return;
+      }
+  
+      if (data.password1 !== data.password2) {
+        showAlertError("Las contraseñas no coinciden");
+        return;
+      }
+  
+      if (!document.getElementById("terminos").checked) {
+        showAlertError("Debe aceptar los términos y condiciones del servicio");
+        return;
+      }
+  
+      showAlertSuccess("Los datos son válidos");
+    });
+  
+    function showAlertSuccess(message) {
+      const alertSuccess = document.getElementById("alert-success");
+      alertSuccess.textContent = message;
+      alertSuccess.classList.add("show");
+    }
+  
+    function showAlertError(message) {
+      const alertError = document.getElementById("alert-danger");
+      alertError.textContent = message;
+      alertError.classList.add("show");
+    }
+  })
